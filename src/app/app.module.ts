9ca1325403d89a9ea557c24d4be90f52d1f2f809
hojpaat <judit.of.horvath@gmail.com>
2024-03-22 10:39:21 +0000

    creating header, footer, trip info components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TripInfoComponent } from './trip-info/trip-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TripInfoComponent
   ],
   imports: [
     BrowserModule,
